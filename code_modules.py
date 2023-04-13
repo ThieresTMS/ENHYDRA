39463662a69e456e419821fa6d59c34d8cc54445
ThieresTMS <thierestayroni@gmail.com>
2023-04-13 16:32:10 -0400

    att

--- code_modules.py
+++ code_modules.py
@@ -4,7 +4,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 import statistics
 import numpy as np
-
+import multiprocessing
 
 def read_config_file(fh_project, fh_code):
     parameters = {}
@@ -14,6 +14,7 @@ def read_config_file(fh_project, fh_code):
     parameters['anchor'] = read_line("anchor", fh_project)
     parameters['mafft'] = read_line("mafft", fh_code)
     parameters['trimal'] = read_line("trimal", fh_code)
+    parameters['max_process'] = int(read_line("max_process", fh_project))
     return parameters
 
 
@@ -49,57 +50,50 @@ def check_parameters(parameters, code_config):
 ##inserir aqui a checagem para o trimmal##
 
 
-def filter_length(parameters, inputfiles):
-    length_out_seq = parameters['outdir'] + "/length_filter"
-    length_out_stats = parameters['outdir'] + "/length_stats"
-    if not os.path.isdir(length_out_seq):
-        os.mkdir(length_out_seq)
-    if not os.path.isdir(length_out_stats):
-        os.mkdir(length_out_stats)
-    for file in inputfiles:
-        inputfile = parameters['inputdir'] + "/" + file
-        if os.stat(inputfile).st_size == 0:
+def filter_length(parameters, file):
+    inputfile = parameters['inputdir'] + "/" + file
+    if os.stat(inputfile).st_size == 0:
+        return
+    outfile_s_path = parameters['outdir'] + "/length_stats" + "/" + file + "_lengthstats"
+    outfile_f_path = parameters['outdir'] + "/length_filter" + "/" + file + "_lengthfilter"
+    outstats = open(outfile_s_path, "a")
+    outfile = open(outfile_f_path, "a")
+    lengths = []
+    length_sum = 0
+    length_data = {}
+    for seq_record in SeqIO.parse(inputfile, "fasta"):
+        seq = Seq(seq_record.seq)
+        seq_id = seq_record.id
+        lenght = len(seq)
+        length_data[seq_id] = lenght
+        lengths.append(lenght)
+        length_sum = length_sum + lenght
+    total_seqs = lengths
+    mean = statistics.mean(lengths)
+    median = statistics.median(lengths)
+    stddev = 0
+    if len(lengths) > 1:
+        stddev = statistics.stdev(lengths)
+    outstats.write("##Overal sequence length stats\n")
+    outstats.write("Total seqs: %s\nAverage: %s\nMedian: %s\nSD: %s\n" % (total_seqs, mean, median, stddev))
+    outstats.write("##Sequence lengths (sorted form smallest to largest)\n")
+    outstats.write("#SequenceID\tLength\tPercentageDifFromAvg\n")
+    keys = list(length_data.keys())
+    values = list(length_data.values())
+    sorted_value_index = np.argsort(values)
+    length_data_sorted = {keys[i]: values[i] for i in sorted_value_index}
+    for key in length_data_sorted:
+        dist = length_data_sorted[key]/mean
+        outstats.write("%s\t%s\t%s\n" % (key, length_data_sorted[key], dist))
+    outstats.close()
+    for seq_record in SeqIO.parse(inputfile, "fasta"):
+        seq = Seq(seq_record.seq)
+        greater = (mean + (2*stddev))
+        smaller = (mean - (2*stddev))
+        if (len(seq) < smaller) or (len(seq) > greater):
             continue
-        outfile_s_path = length_out_stats + "/" + file + "_lengthstats"
-        outfile_f_path = length_out_seq + "/" + file + "_lengthfilter"
-        outstats = open(outfile_s_path, "a")
-        outfile = open(outfile_f_path, "a")
-        lengths = []
-        length_sum = 0
-        length_data = {}
-        for seq_record in SeqIO.parse(inputfile, "fasta"):
-            seq = Seq(seq_record.seq)
-            seq_id = seq_record.id
-            lenght = len(seq)
-            length_data[seq_id] = lenght
-            lengths.append(lenght)
-            length_sum = length_sum + lenght
-        total_seqs = lengths
-        mean = statistics.mean(lengths)
-        median = statistics.median(lengths)
-        stddev = 0
-        if len(lengths) > 1:
-            stddev = statistics.stdev(lengths)
-        outstats.write("##Overal sequence length stats\n")
-        outstats.write("Total seqs: %s\nAverage: %s\nMedian: %s\nSD: %s\n" % (total_seqs, mean, median, stddev))
-        outstats.write("##Sequence lengths (sorted form smallest to largest)\n")
-        outstats.write("#SequenceID\tLength\tPercentageDifFromAvg\n")
-        keys = list(length_data.keys())
-        values = list(length_data.values())
-        sorted_value_index = np.argsort(values)
-        length_data_sorted = {keys[i]: values[i] for i in sorted_value_index}
-        for key in length_data_sorted:
-            dist = length_data_sorted[key]/mean
-            outstats.write("%s\t%s\t%s\n" % (key, length_data_sorted[key], dist))
-        outstats.close()
-        for seq_record in SeqIO.parse(inputfile, "fasta"):
-            seq = Seq(seq_record.seq)
-            greater = (mean + (2*stddev))
-            smaller = (mean - (2*stddev))
-            if (len(seq) < smaller) or (len(seq) > greater):
-                continue
-            else:
-                outfile.write(">%s\n%s\n" % (seq_record.id, seq))
+        else:
+            outfile.write(">%s\n%s\n" % (seq_record.id, seq))
 
 
 def filter_groups(parameters, outlog):
@@ -137,7 +131,7 @@ def run_mafft(parameters):
     for file in input_files:
         input_seq = dirpath + "/" + file
         output_seq = outdir + "/" + file + ".aln"
-        os.system('%s --auto --quiet  %s > %s' % (parameters['mafft'], input_seq, output_seq))
+        os.system('%s --auto --quiet --thread %s %s > %s' % (parameters['mafft'], parameters['max_process'], input_seq, output_seq))
 
 
 def run_trimal(parameters):
@@ -156,23 +150,16 @@ def make_tables(parameters):
     tables = parameters['outdir'] + "/tables"
     if not os.path.isdir(tables):
         os.mkdir(tables)
+    ortho_mean = {}
     dirpath1 = parameters['outdir'] + "/alignment"
     input_files1 = os.listdir(dirpath1)
     group2anchor_path = tables + "/group2anchor.tsv"
     group2anchor= open(group2anchor_path, "a")
-    for file1 in input_files1:
-        aux = file1.split(".")
-        group_name = aux[0]
-        seq_file = dirpath1 + '/' + file1
-        for seq_record in SeqIO.parse(seq_file, "fasta"):
-            ids_fields = seq_record.id.split("|")
-            specie = ids_fields[0]
-            sequence_id = ids_fields[1]
-            if specie == parameters['anchor']:
-                group2anchor.write("%s\t%s\n" % (group_name, sequence_id))
     dirpath2 = parameters['outdir'] + "/ident_alignment"
     input_files2 = os.listdir(dirpath2)
     group2mean_path = tables + "/group2mean.tsv"
+    anchor2mean_path = tables + "/anchor2mean.tsv"
+    anchor2mean = open(anchor2mean_path, "a")
     group2mean = open(group2mean_path, "a")
     for file2 in input_files2:
         aux2 = file2.split(".")
@@ -185,7 +172,16 @@ def make_tables(parameters):
             if match:
                 values = line.split()
                 mean_percent = values[-1]
+                ortho_mean[group_name2] = mean_percent
                 group2mean.write("%s\t%s\n" % (group_name2, mean_percent))
-
-
-
+    for file1 in input_files1:
+        aux = file1.split(".")
+        group_name = aux[0]
+        seq_file = dirpath1 + '/' + file1
+        for seq_record in SeqIO.parse(seq_file, "fasta"):
+            ids_fields = seq_record.id.split("|")
+            specie = ids_fields[0]
+            sequence_id = ids_fields[1]
+            if specie == parameters['anchor']:
+                anchor2mean.write("%s\t%s\n" % (sequence_id, ortho_mean[group_name]))
+                group2anchor.write("%s\t%s\n" % (group_name, sequence_id))
